8bd5ced59da8063a94abdf18add2acf651abbb92
MinhThang <dominhtoan9467@gmail.com>
2025-10-28 09:04:53 +0700

    Hẹ hhẹ

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -13,5 +13,4 @@ Console.WriteLine("hom nay troi dep qua");
 
 Console.WriteLine("hom nay troi dep qua");
 
-Console.WriteLine("hom nay troi dep qua");
-Console.WriteLine("hom nay troi dep qua");
+Console.WriteLine("HEEEEEEEEEEEEEEEHHEEEEEEEEEe");
